edc76b2beb3265aa67bdbac6380c0ac4f842c080
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-01-29 16:16:53 -0800

    Add analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,3 +139,21 @@ $(function(){
   }
   document.body.className += " math_finished";
 });
+
+
+// Analytics
+
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-54996-14', 'auto');
+ga('send', 'pageview');
