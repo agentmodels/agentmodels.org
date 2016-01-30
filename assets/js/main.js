c4cfe0ea13abcab707a115a6d82c463547b86f4d
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-01-29 16:17:22 -0800

    Whitespace

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,7 @@ function github_page_url(page_url) {
 
 
 // WebPPL editor
+
 $(function(){
   var preEls = Array.prototype.slice.call(document.querySelectorAll("pre"));
   preEls.map(function(el) { wpCodeEditor(el, {language: 'webppl'}); });          
